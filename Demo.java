fec34090d5a425e36a4662864a1577379a86302e
sujinwhiz <sujinsubin2005@gmail.com>
2024-05-20 22:43:42 +0530

    modified java file

--- Demo.java
+++ Demo.java
@@ -5,6 +5,6 @@ public static void main(String[] args){
     for(int i=1;i<=num;i++){
       if(num%i==0) count++;
     }
-    if(count == 2) System.out.println("PRIME NUMBER");
+    if(count == 2) System.out.println(num+"  PRIME NUMBER");
   }
 }}
